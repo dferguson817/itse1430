f1c2dd4198d0c3bfd6112b9e032c19429714f72c
dustin.ferguson <dustin.ferguson@my.tccd.edu>
2020-02-03 21:43:54 -0600

    Movie Library

---
+++ classwork/Section1/Movie Library/Movie Library.Business/Movie.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Movie_Library.Business
+{
+    public class Movie
+    {
+
+        string title;
+
+        string runLength;
+
+        string description;
+
+        string releaseYear;
+
+        string classic;
+    }
+}
---
+++ classwork/Section1/Movie Library/Movie Library/MainForm.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Movie_Library.Business;
+
+namespace Movie_Library
+{
+    public partial class MainForm : Form
+    {
+        public MainForm ()
+        {
+            InitializeComponent();
+
+            Movie movie;
+        }
+    }
+}
